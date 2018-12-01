5adf837318a7adc4b6887fdf36cb03a2f9532755
Kameron Robinson <kamdroidR@me.com>
2018-12-01 16:06:48 -0500

    resolve children

--- server/src/resolvers/User.js
+++ server/src/resolvers/User.js
@@ -2,6 +2,9 @@ const User = {
   posts: (parent, args, context) => {
     return context.prisma.user({ id: parent.id }).posts()
   },
+  children: (parent, args, context) => {
+    return context.prisma.user({ id: parent.id }).children()
+  },
 }
 
 module.exports = {
