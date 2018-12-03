dc3a953bccb865b0d1ad4cac7843cb10ca7b8c1a
Kameron Robinson <kamdroidR@me.com>
2018-12-02 21:26:06 -0500

    lint

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Redirect,
   Route,
   Switch,
 } from 'react-router-dom';
--- client/src/screens/ChildInfo/index.js
+++ client/src/screens/ChildInfo/index.js
@@ -1,9 +1,4 @@
-/**
-* Description here...
-*/
 import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
 const ChildInfo = () => (
   <div>ChildInfo</div>
