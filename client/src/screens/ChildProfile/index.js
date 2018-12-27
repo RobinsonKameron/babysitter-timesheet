67a9bb397181cb96e04f264a3ac3ee5937faee72
Kameron Robinson <kamdroidR@me.com>
2018-12-26 21:12:46 -0500

    Update child Profile

--- client/src/screens/ChildProfile/index.js
+++ client/src/screens/ChildProfile/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Avatar } from 'antd';
+import { withRouter } from 'react-router-dom';
+import { Query } from 'react-apollo';
+import { SITTE_QUERY } from '../../graphql/queries/SITTE_QUERY';
 
-const Child = () => (
-  <div>Child</div>
+const getShortName = str => str.charAt(0);
+
+
+const Child = ({ match }) => (
+  <Query
+    variables={{
+      where: {
+        id: match.params.id,
+      },
+    }}
+    query={SITTE_QUERY}
+  >
+    {({ data, loading, error }) => {
+      if (loading) {
+        return 'loading...';
+      }
+      if (error) {
+        return 'Something went wrong';
+      }
+      return (
+        <div>
+          <Avatar>
+            {getShortName(data.sitte.firstName)}
+            {getShortName(data.sitte.lastName)}
+          </Avatar>
+        </div>
+      );
+    }}
+  </Query>
 );
 
-export default Child;
+Child.propTypes = {
+  match: PropTypes.shape({
+    id: PropTypes.string,
+  }).isRequired,
+};
+
+export default withRouter(Child);
