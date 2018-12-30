7a8ee0aff54a1b359f5eef216c93bff658f07a75
Kameron Robinson <kamdroidR@me.com>
2018-12-30 12:29:55 -0500

    add Mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -2,12 +2,14 @@ const auth = require('./mutations/auth');
 const createSittee = require('./mutations/createSitte');
 const createOrUpdateDate = require('./mutations/createOrUpdateDate');
 const stripe = require("./mutations/Stripe");
+const allergy = require("./mutations/allergy");
 
 const Mutation = {
   ...auth,
   ...createSittee,
   ...createOrUpdateDate,
-  ...stripe
+  ...stripe,
+  ...allergy
 }
 
 module.exports = {
