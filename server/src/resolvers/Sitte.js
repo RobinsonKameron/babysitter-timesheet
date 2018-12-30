a1bdd1004cd863459b0f30852265c49e87e362ac
Kameron Robinson <kamdroidR@me.com>
2018-12-30 12:30:07 -0500

    add owner to sitte

--- server/src/resolvers/Sitte.js
+++ server/src/resolvers/Sitte.js
@@ -2,7 +2,8 @@ const { getUserId } = require("../utils");
 
 const Sitte = {
   dates: async ({ id }, _, ctx) => await ctx.prisma.sitte({ id }).dates(),
-  owner: async ({ id }, _, ctx) => await ctx.prisma.sitte({ id }).owner()
+  owner: async ({ id }, _, ctx) => await ctx.prisma.sitte({ id }).owner(),
+  allergies: async ({ id }, _, ctx) => await ctx.prisma.sitte({ id }).allergies(),
 };
 
 module.exports = {
